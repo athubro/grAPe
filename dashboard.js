85087e1477ae2e8027901e5cce44601d0ae413df
athubro <abengeri1@gmail.com>
2025-08-31 23:32:38 -0400

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -101,6 +101,22 @@ Output JSON.
 
 // ========================= AP Courses =========================
 const apCourses = {
+  "AP African American Studies": ["Unit 1: Introduction to African American Studies"],
+  "AP Art & Design: 2D": ["Unit 1: Explore Materials, Processes, and Ideas", "Unit 2: Make Art and Design", "Unit 3: Present Art and Design"],
+  "AP Art & Design: 3D": ["Unit 1: Explore Materials, Processes, and Ideas", "Unit 2: Make Art and Design", "Unit 3: Present Art and Design"],
+  "AP Art & Design: Drawing": ["Unit 1: Explore Materials, Processes, and Ideas", "Unit 2: Make Art and Design", "Unit 3: Present Art and Design"],
+  "AP Art History": [
+    "Unit 1: Global Prehistoric Art, 30,000-500 BCE",
+    "Unit 2: Ancient Mediterranean Art, 3500-300 BCE",
+    "Unit 3: Early European and Colonial American Art, 200-1750 CE",
+    "Unit 4: Later European and American Art, 1750-1980 CE",
+    "Unit 5: Indigenous American Art, 1000 BCE-1980 CE",
+    "Unit 6: African Art, 1100-1980 CE",
+    "Unit 7: West and Central Asian Art, 500 BCE-1980 CE",
+    "Unit 8: South, East, and Southeast Asian Art, 300 BCE-1980 CE",
+    "Unit 9: Art in the Pacific, 700-1980 CE",
+    "Unit 10: Global Contemporary Art, 1980 CE to Present"
+  ],
   "AP Biology": [
     "Unit 1: Chemistry of Life",
     "Unit 2: Cell Structure and Function",
@@ -113,15 +129,316 @@ const apCourses = {
   ],
   "AP Calculus AB": [
     "Unit 1: Limits and Continuity",
-    "Unit 2: Differentiation",
-    "Unit 3: Composite & Implicit Functions",
-    "Unit 4: Applications of Differentiation",
-    "Unit 5: Integration",
-    "Unit 6: Differential Equations",
-    "Unit 7: Applications of Integration"
+    "Unit 2: Differentiation - Definition and Fundamental Properties",
+    "Unit 3: Differentiation - Composite, Implicit, and Inverse Functions",
+    "Unit 4: Contextual Applications of Differentiation",
+    "Unit 5: Analytical Applications of Differentiation",
+    "Unit 6: Integration and Accumulation of Change",
+    "Unit 7: Differential Equations",
+    "Unit 8: Applications of Integration"
+  ],
+  "AP Calculus BC": [
+    "Unit 1: Limits and Continuity",
+    "Unit 2: Differentiation - Definition and Fundamental Properties",
+    "Unit 3: Differentiation - Composite, Implicit, and Inverse Functions",
+    "Unit 4: Contextual Applications of Differentiation",
+    "Unit 5: Analytical Applications of Differentiation",
+    "Unit 6: Integration and Accumulation of Change",
+    "Unit 7: Differential Equations",
+    "Unit 8: Applications of Integration",
+    "Unit 9: Parametric Equations, Polar Coordinates, and Vector-Valued Functions",
+    "Unit 10: Infinite Sequences and Series"
+  ],
+  "AP Chemistry": [
+    "Unit 1: Atomic Structure and Properties",
+    "Unit 2: Molecular and Ionic Bonding",
+    "Unit 3: Intermolecular Forces and Properties",
+    "Unit 4: Chemical Reactions",
+    "Unit 5: Kinetics",
+    "Unit 6: Thermodynamics",
+    "Unit 7: Equilibrium",
+    "Unit 8: Acids and Bases",
+    "Unit 9: Applications of Thermodynamics"
+  ],
+  "AP Chinese": [
+    "Unit 1: Families in Different Societies",
+    "Unit 2: The Influence of Language and Culture on Identity",
+    "Unit 3: Influences of Beauty and Art",
+    "Unit 4: How Science and Technology Affect Our Lives",
+    "Unit 5: Factors That Impact the Quality of Life",
+    "Unit 6: Environmental, Political, and Societal Challenges"
+  ],
+  "AP Comparative Government": [
+    "Unit 1: Political Systems, Regimes, and Governments",
+    "Unit 2: Political Institutions",
+    "Unit 3: Political Culture and Participation",
+    "Unit 4: Party, Electoral Systems, and Citizen Organizations",
+    "Unit 5: Political and Economic Changes and Development"
+  ],
+  "AP Computer Science A": [
+    "Unit 1: Primitive Types",
+    "Unit 2: Using Objects",
+    "Unit 3: Boolean Expressions and If Statements",
+    "Unit 4: Iteration",
+    "Unit 5: Writing Classes",
+    "Unit 6: Arrays",
+    "Unit 7: ArrayLists",
+    "Unit 8: 2D Arrays",
+    "Unit 9: Inheritance",
+    "Unit 10: Recursion"
+  ],
+  "AP Computer Science Principles": [
+    "Unit 1: Creative Development",
+    "Unit 2: Data",
+    "Unit 3: Algorithms and Programming",
+    "Unit 4: Computer Systems and Networks",
+    "Unit 5: Impact of Computing"
+  ],
+  "AP English Language": [
+    "Unit 1: Claims, Reasoning, and Evidence",
+    "Unit 2: Organizing Information for a Specific Audience",
+    "Unit 3: Perspectives and How Arguments Relate",
+    "Unit 4: How Writers Develop Arguments, Intros, and Conclusions",
+    "Unit 5: How a Writer Brings All Parts of an Argument Together",
+    "Unit 6: Position, Perspective, and Bias",
+    "Unit 7: Successful and Unsuccessful Arguments",
+    "Unit 8: Stylistic Choices",
+    "Unit 9: Developing a Complex Argument"
+  ],
+  "AP English Literature": [
+    "Unit 1: Short Fiction I: Introduction to Short Fiction",
+    "Unit 2: Poetry I: Introduction to Poetry",
+    "Unit 3: Longer Fiction and Drama I: Introduction to Longer Fiction and Drama",
+    "Unit 4: Short Fiction II: Character, Conflict, and Storytelling",
+    "Unit 5: Poetry II: Structure and Figurative Language",
+    "Unit 6: Longer Fiction or Drama II: Literary Techniques in Longer Works",
+    "Unit 7: Short Fiction III: Societal and Historical Context",
+    "Unit 8: Poetry III: Contrast, Ambiguous Language, and Other Techniques",
+    "Unit 9: Longer Fiction or Drama III: Nuanced Analysis"
+  ],
+  "AP Environmental Science": [
+    "Unit 1: The Living World: Ecosystems",
+    "Unit 2: The Living World: Biodiversity",
+    "Unit 3: Populations",
+    "Unit 4: Earth Systems and Resources",
+    "Unit 5: Land and Water Use",
+    "Unit 6: Energy Resources and Consumption",
+    "Unit 7: Atmospheric Pollution",
+    "Unit 8: Aquatic and Terrestrial Pollution",
+    "Unit 9: Global Change"
+  ],
+  "AP European History": [
+    "Unit 1: Renaissance and Exploration",
+    "Unit 2: Age of Reformation",
+    "Unit 3: Absolutism and Constitutionalism",
+    "Unit 4: Scientific, Philosophical, and Political Developments",
+    "Unit 5: Conflict, Crisis, and Reaction in the Late 18th Century",
+    "Unit 6: Industrialization and Its Effects",
+    "Unit 7: 19th Century Perspectives and Political Developments",
+    "Unit 8: 20th Century Global Conflicts",
+    "Unit 9: Cold War and Contemporary Europe"
+  ],
+  "AP French": [
+    "Unit 1: Families in Different Societies",
+    "Unit 2: The Influence of Language and Culture on Identity",
+    "Unit 3: Influences of Beauty and Art",
+    "Unit 4: How Science and Technology Affect Our Lives",
+    "Unit 5: Factors That Impact the Quality of Life",
+    "Unit 6: Environmental, Political, and Societal Challenges"
+  ],
+  "AP German": [
+    "Unit 1: Families in Different Societies",
+    "Unit 2: The Influence of Language and Culture on Identity",
+    "Unit 3: Influences of Beauty and Art",
+    "Unit 4: How Science and Technology Affect Our Lives",
+    "Unit 5: Factors That Impact the Quality of Life",
+    "Unit 6: Environmental, Political, and Societal Challenges"
+  ],
+  "AP Human Geography": [
+    "Unit 1: Thinking Geographically",
+    "Unit 2: Population and Migration Patterns and Processes",
+    "Unit 3: Cultural Patterns and Processes",
+    "Unit 4: Political Patterns and Processes",
+    "Unit 5: Agriculture and Rural Land-Use Patterns and Processes",
+    "Unit 6: Cities and Urban Land-Use",
+    "Unit 7: Industrial and Economic Development Patterns and Processes"
+  ],
+  "AP Italian": [
+    "Unit 1: Families in Different Societies",
+    "Unit 2: The Influence of Language and Culture on Identity",
+    "Unit 3: Influences of Beauty and Art",
+    "Unit 4: How Science and Technology Affect Our Lives",
+    "Unit 5: Factors That Impact the Quality of Life",
+    "Unit 6: Environmental, Political, and Societal Challenges"
+  ],
+  "AP Japanese": [
+    "Unit 1: Families in Different Societies",
+    "Unit 2: The Influence of Language and Culture on Identity",
+    "Unit 3: Influences of Beauty and Art",
+    "Unit 4: How Science and Technology Affect Our Lives",
+    "Unit 5: Factors That Impact the Quality of Life",
+    "Unit 6: Environmental, Political, and Societal Challenges"
+  ],
+  "AP Latin": [
+    "Unit 1: Vergil, Aeneid, Book 1",
+    "Unit 2: Vergil, Aeneid, Book 2",
+    "Unit 3: Vergil, Aeneid, Book 4",
+    "Unit 4: Vergil, Aeneid, Book 6",
+    "Unit 5: Catullus, Selected Poems",
+    "Unit 6: Caesar, Selected Passages",
+    "Unit 7: Cicero, Selected Speeches"
+  ],
+  "AP Macroeconomics": [
+    "Unit 1: Basic Economic Concepts",
+    "Unit 2: Economic Indicators and the Business Cycle",
+    "Unit 3: National Income and Price Determination",
+    "Unit 4: Financial Sector",
+    "Unit 5: Long-Run Consequences of Stabilization Policies",
+    "Unit 6: Open Economy - International Trade and Finance"
+  ],
+  "AP Microeconomics": [
+    "Unit 1: Basic Economic Concepts",
+    "Unit 2: Supply and Demand",
+    "Unit 3: Production, Cost, and the Perfect Competition Model",
+    "Unit 4: Imperfect Competition",
+    "Unit 5: Factor Markets",
+    "Unit 6: Market Failure and the Role of Government"
+  ],
+  "AP Music Theory": [
+    "Unit 1: Music Fundamentals I",
+    "Unit 2: Music Fundamentals II",
+    "Unit 3: Music Fundamentals III",
+    "Unit 4: Harmony and Voice Leading I",
+    "Unit 5: Harmony and Voice Leading II",
+    "Unit 6: Harmony and Voice Leading III",
+    "Unit 7: Harmony and Voice Leading IV",
+    "Unit 8: Modes and Form"
+  ],
+  "AP Physics 1": [
+    "Unit 1: Kinematics",
+    "Unit 2: Dynamics",
+    "Unit 3: Circular Motion and Gravitation",
+    "Unit 4: Energy",
+    "Unit 5: Momentum",
+    "Unit 6: Simple Harmonic Motion",
+    "Unit 7: Torque and Rotational Motion"
+  ],
+  "AP Physics 2": [
+    "Unit 1: Fluids",
+    "Unit 2: Thermodynamics",
+    "Unit 3: Electric Force, Field, and Potential",
+    "Unit 4: Electric Circuits",
+    "Unit 5: Magnetism and Electromagnetic Induction",
+    "Unit 6: Geometric and Physical Optics",
+    "Unit 7: Quantum, Atomic, and Nuclear Physics"
+  ],
+  "AP Physics C: Electricity & Magnetism": [
+    "Unit 1: Electrostatics",
+    "Unit 2: Conductors, Capacitors, Dielectrics",
+    "Unit 3: Electric Circuits",
+    "Unit 4: Magnetic Fields",
+    "Unit 5: Electromagnetism"
+  ],
+  "AP Physics C: Mechanics": [
+    "Unit 1: Kinematics",
+    "Unit 2: Newton’s Laws of Motion",
+    "Unit 3: Work, Energy, and Power",
+    "Unit 4: Systems of Particles and Linear Momentum",
+    "Unit 5: Rotation",
+    "Unit 6: Oscillations",
+    "Unit 7: Gravitation"
+  ],
+  "AP Pre-Calculus": [
+    "Unit 1: Polynomial and Rational Functions",
+    "Unit 2: Exponential and Logarithmic Functions",
+    "Unit 3: Trigonometric and Polar Functions",
+    "Unit 4: Functions Involving Parameters, Vectors, and Matrices"
+  ],
+  "AP Psychology": [
+    "Unit 1: Scientific Foundations of Psychology",
+    "Unit 2: Biological Basis of Behavior",
+    "Unit 3: Sensation and Perception",
+    "Unit 4: Learning",
+    "Unit 5: Cognitive Psychology",
+    "Unit 6: Developmental Psychology",
+    "Unit 7: Motivation, Emotion, and Personality",
+    "Unit 8: Clinical Psychology",
+    "Unit 9: Social Psychology"
+  ],
+  "AP Research": [
+    "Unit 1: Question and Explore",
+    "Unit 2: Understand and Analyze",
+    "Unit 3: Evaluate Multiple Perspectives",
+    "Unit 4: Synthesize Ideas",
+    "Unit 5: Team, Transform, and Transmit"
+  ],
+  "AP Seminar": [
+    "Unit 1: Question and Explore",
+    "Unit 2: Understand and Analyze",
+    "Unit 3: Evaluate Multiple Perspectives",
+    "Unit 4: Synthesize Ideas",
+    "Unit 5: Team, Transform, and Transmit"
+  ],
+  "AP Spanish Language": [
+    "Unit 1: Families in Different Societies",
+    "Unit 2: The Influence of Language and Culture on Identity",
+    "Unit 3: Influences of Beauty and Art",
+    "Unit 4: How Science and Technology Affect Our Lives",
+    "Unit 5: Factors That Impact the Quality of Life",
+    "Unit 6: Environmental, Political, and Societal Challenges"
+  ],
+  "AP Spanish Literature": [
+    "Unit 1: La época medieval",
+    "Unit 2: El siglo XVI",
+    "Unit 3: El siglo XVII",
+    "Unit 4: La literatura romántica, realista y naturalista",
+    "Unit 5: La Generación del 98 y el Modernismo",
+    "Unit 6: Teatro y poesía del siglo XX",
+    "Unit 7: El Boom latinoamericano",
+    "Unit 8: Escritores contemporáneos de Estados Unidos, y España"
+  ],
+  "AP Statistics": [
+    "Unit 1: Exploring One-Variable Data",
+    "Unit 2: Exploring Two-Variable Data",
+    "Unit 3: Collecting Data",
+    "Unit 4: Probability, Random Variables, and Probability Distributions",
+    "Unit 5: Sampling Distributions",
+    "Unit 6: Inference for Categorical Data - Proportions",
+    "Unit 7: Inference for Quantitative Data - Means",
+    "Unit 8: Inference for Categorical Data - Chi-Squares",
+    "Unit 9: Inference for Quantitative Data - Slopes"
+  ],
+  "AP US Government": [
+    "Unit 1: Foundations of American Democracy",
+    "Unit 2: Interactions Among Branches of Government",
+    "Unit 3: Civil Liberties and Civil Rights",
+    "Unit 4: American Political Ideologies and Beliefs",
+    "Unit 5: Political Participation"
+  ],
+  "AP US History": [
+    "Unit 1: 1491-1607 (Columbus to Jamestown)",
+    "Unit 2: 1607-1754 (Colonial Society)",
+    "Unit 3: 1754-1800 (American Revolution and Constitution)",
+    "Unit 4: 1800-1848 (American Expansion)",
+    "Unit 5: 1844-1877 (Civil War and Reconstruction)",
+    "Unit 6: 1865-1898 (Gilded Age)",
+    "Unit 7: 1898-1945 (Global Conflict)",
+    "Unit 8: 1945-1980 (Cold War and Protest)",
+    "Unit 9: 1980 to Present (Post-Cold War Shifts)"
+  ],
+  "AP World History: Modern": [
+    "Unit 1: The Global Tapestry",
+    "Unit 2: Networks of Exchange",
+    "Unit 3: Land-Based Empires",
+    "Unit 4: Transoceanic Interactions",
+    "Unit 5: Revolutions",
+    "Unit 6: Consequences of Industrialization",
+    "Unit 7: Global Conflict",
+    "Unit 8: Cold War and Decolonization",
+    "Unit 9: Globalization"
   ]
-  // Add more courses...
 };
+
 
 // ========================= DOM Elements =========================
 const welcome = document.getElementById("welcome");
