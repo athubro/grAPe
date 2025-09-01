98fb755c631163ed3ed4cf376f474704fea96452
athubro <abengeri1@gmail.com>
2025-09-01 10:20:07 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
-import { initializeApp, getApps } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { 
   getAuth, 
-  onAuthStateChanged, 
+  signInWithEmailAndPassword, 
+  GoogleAuthProvider, 
+  signInWithPopup, 
   setPersistence, 
-  browserLocalPersistence, 
-  signOut 
+  browserLocalPersistence 
 } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
-import { getFirestore, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
 // Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyAekCu-kTFwWcxT0UPy58nXlt8ZNA0VsLI",
+  apiKey: "AIzaSyCdOUtoPjAHyXoxBJPJvAVsveMuPA2vUSQ",
   authDomain: "grape-mcps.firebaseapp.com",
   projectId: "grape-mcps",
   storageBucket: "grape-mcps.firebasestorage.app",
   messagingSenderId: "909399056268",
-  appId: "1:909399056268:web:3ac13a43d1e1846649c0a9"
+  appId: "1:909399056268:web:3ac13a43d1e1846649c0a9",
+  measurementId: "G-X2DELV9RFD"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
